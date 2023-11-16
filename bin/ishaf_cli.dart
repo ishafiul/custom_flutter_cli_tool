1bd09fd1936005983243d91bc51e13d481cdb302
ishaf <shafiulislam20@gmail.com>
2023-11-16 11:18:04 +0600

    cmd updated

--- bin/ishaf_cli.dart
+++ bin/ishaf_cli.dart
@@ -2,7 +2,7 @@ import 'package:args/command_runner.dart';
 import 'package:ishaf_cli/commands/create.dart';
 
 Future<void> main(List<String> args) async {
-  final runner = CommandRunner('ipp', 'Official CLI for IPP xD')
+  final runner = CommandRunner('ishaf', 'Official ishaf CLI xD')
     ..addCommand(CreateCommand());
 
   await runner.run(args);
